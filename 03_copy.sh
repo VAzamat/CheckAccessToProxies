5fa8a74a3b7754faa7219345e96f5cf1e845823c
Valeev Azamat <vazamat@gmail.com>
2026-06-25 19:21:21 +0300

    today file with proxies 20260625

--- 03_copy.sh
+++ 03_copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f prooven_proxies.dat  socks4.txt  socks5.txt
+rm -f prooven_proxies.dat  socks4.txt  socks5.txt usaproxy.txt
 sort prooven_proxies_yt.dat > "`date +%Y%m%d`prooven_proxies_yt.dat"
